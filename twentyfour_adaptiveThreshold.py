bb6180939dcd942cd742f5ccfe0797c6bff04369
xwwwb <xuwenbo@88.com>
2022-09-30 22:04:40 +0800

    自适应二值化

--- twentyfour_adaptiveThreshold.py
+++ twentyfour_adaptiveThreshold.py
@@ -4,9 +4,12 @@
 
 import cv2
 
-img = cv2.imread('./resource/demo.jpg')
+img = cv2.imread('./resource/demo2.jpg', cv2.IMREAD_GRAYSCALE)
 
-dst = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,25,10)
+dst = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 0)
+
+cv2.imshow('img', img)
+cv2.imshow('dst', dst)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
